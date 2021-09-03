1a1151dfd1a1c2d882191700a090deb256e2b8c2
Rafael Friederick <rafael.mf.documents@gmail.com>
2021-09-03 13:56:18 -0300

    Requisito 1 Finalizado

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+  request.user = user;
+  return (
+    user
+    ? next()
+    : response.status(404).json({ error: "Usuario Inexistente" }).send()
+  );
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
